d0088f8c415695df5e18d9a956699cfb24573ae7
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-08-11 13:20:07 +0800

    CAT

--- src/main/java/optifinecitpatch/ConfigHandler.java
+++ src/main/java/optifinecitpatch/ConfigHandler.java
@@ -1,33 +1,43 @@
 package optifinecitpatch;
 
-import fermiumbooter.annotations.MixinConfig;
+import com.cleanroommc.configanytime.ConfigAnytime;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = OptiFineCITPatch.MODID)
-@MixinConfig(name = OptiFineCITPatch.MODID)
-@SuppressWarnings("unused")
+@Mod.EventBusSubscriber(modid = OptiFineCITPatch.MODID)
 public class ConfigHandler {
     @Config.Comment("Enchanted Books will not have Enchantment Glint if set to true.")
     @Config.Name("Remove Glint")
     @Config.RequiresMcRestart
-    @MixinConfig.MixinToggle(earlyMixin = "mixins.optifinecitpatch.glint.json", defaultValue = false)
     public static boolean removeGlint = false;
 
     @Config.Comment("Will not spam the startup log with one line per registered CIT if enabled.")
     @Config.Name("Remove Log Spam")
     @Config.RequiresMcRestart
-    @MixinConfig.MixinToggle(lateMixin = "mixins.optifinecitpatch.logspam.json", defaultValue = true)
     public static boolean removeLogSpam = true;
 
     @Config.Comment("Will only render a CIT for the first enchant on a book with multiple enchantments.")
     @Config.Name("Only First Enchant")
     @Config.RequiresMcRestart
-    @MixinConfig.MixinToggle(lateMixin = "mixins.optifinecitpatch.firstenchant.json", defaultValue = true)
     public static boolean onlyFirstEnchant = true;
 
     @Config.Comment("Fixes CITs breaking completely if an enchantment couldn't be found.")
     @Config.Name("Fix Missing Enchantment")
     @Config.RequiresMcRestart
-    @MixinConfig.MixinToggle(lateMixin = "mixins.optifinecitpatch.fixmissing.json", defaultValue = true)
     public static boolean fixMissingEnchantment = true;
+
+    static {
+        ConfigAnytime.register(ConfigHandler.class);
+    }
+
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(OptiFineCITPatch.MODID)) {
+            ConfigManager.sync(OptiFineCITPatch.MODID, Config.Type.INSTANCE);
+        }
+    }
 }
--- src/main/java/optifinecitpatch/OptiFineCITPatchPlugin.java
+++ src/main/java/optifinecitpatch/OptiFineCITPatchPlugin.java
@@ -1,16 +1,16 @@
 package optifinecitpatch;
 
+import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.spongepowered.asm.launch.MixinBootstrap;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
-public class OptiFineCITPatchPlugin implements IFMLLoadingPlugin {
-
-	public OptiFineCITPatchPlugin() {
-		MixinBootstrap.init();
-	}
+public class OptiFineCITPatchPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
 	@Override
 	public String[] getASMTransformerClass()
@@ -37,5 +37,40 @@ public class OptiFineCITPatchPlugin implements IFMLLoadingPlugin {
 	public String getAccessTransformerClass()
 	{
 		return null;
+	}
+
+	@Override
+	public List<String> getMixinConfigs() {
+		List<String> mixins = new ArrayList<>();
+
+		if (ConfigHandler.removeGlint) {
+			mixins.add("mixins.optifinecitpatch.glint.json");
+		}
+		if (isOptiFineLoaded()){
+			if (ConfigHandler.removeLogSpam){
+				mixins.add("mixins.optifinecitpatch.logspam.json");
+			}
+			if (ConfigHandler.onlyFirstEnchant) {
+				mixins.add("mixins.optifinecitpatch.firstenchant.json");
+			}
+			if (ConfigHandler.fixMissingEnchantment) {
+				mixins.add("mixins.optifinecitpatch.fixmissing.json");
+			}
+		}
+
+		return mixins;
+	}
+
+	public static boolean isOptiFineLoaded() {
+		try {
+			Method method = ClassLoader.class.getDeclaredMethod("findLoadedClass", String.class);
+			method.setAccessible(true);
+			boolean isPresent = method.invoke(Launch.classLoader, "optifine.OptiFineClassTransformer") != null;
+			if (isPresent)
+				OptiFineCITPatch.LOGGER.info("OptiFine CIT Patch: The following PACKAGE_CLASSLOADER_EXCLUSION error is fine, it actually shows that it works.");
+			return isPresent;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
